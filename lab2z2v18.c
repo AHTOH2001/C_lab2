35e3d18b9cd82ee4a4a478d585c4263c38251385
AHTOH2001 <antonlapytko@gmail.com>
2020-02-19 21:25:03 +0300

    try8

--- lab2z2v18.c
+++ lab2z2v18.c
@@ -8,59 +8,82 @@
 #define max 9223372036854775807
 #define PI 3.14159265358979323846
 
-double myabs(double x) {
-    if (x < 0) return -x;
-    else return x;
+double myabs(double x)
+{
+    if (x < 0)
+        return -x;
+    else
+        return x;
 }
 
-double stod(char s[100]) {
+double stod(char s[100])
+{
     int i = 0, o = false, f = false;
     double sm = 0, d = 1;
-    if (s[0] == '-') {
+    if (s[0] == '-')
+    {
         o = true;
         i++;
     }
-    if (s[0] == '\0' || s[0] == '\n') return 0;
-    while (s[i] != '\0' && s[i] != '\n') {
-        if (f) d *= 10;
-        if (s[i] >= '0' && s[i] <= '9') sm = sm * 10 + (s[i] - '0');
-        else if (s[i] == '.' && !f) f = true;
-        else return err;
+    if (s[0] == '\0' || s[0] == '\n')
+        return 0;
+    while (s[i] != '\0' && s[i] != '\n')
+    {
+        if (f)
+            d *= 10;
+        if (s[i] >= '0' && s[i] <= '9')
+            sm = sm * 10 + (s[i] - '0');
+        else if (s[i] == '.' && !f)
+            f = true;
+        else
+            return err;
 
-        if (sm >= max / 100) return err;
+        if (sm >= max / 100)
+            return err;
         i++;
     }
-    if (o == true) return -1 * sm / d;
-    else return sm / d;
+    if (o == true)
+        return -1 * sm / d;
+    else
+        return sm / d;
 }
 
-void problem2() {
+void problem2()
+{
     printf("vvedite x and eps\n");
     double x, eps, sum = 0, fc = 1;
     char s1[1000] = {'\0'}, s2[1000] = {'\0'};
     scanf("%s %s", &s1, &s2);
     x = stod(s1);
     eps = stod(s2);
-    if (x == err || eps == err) {
+    if (x == err || eps == err)
+    {
         printf("error");
         return;
     }
-    if (eps <= 0) {
+    if (eps <= 0)
+    {
         printf("err: epsilon should be more than 0");
         return;
     }
-    if (myabs(x) >= 1000) {
+    if (myabs(x) >= 1000)
+    {
         printf("err: x too big");
         return;
     }
     printf("right sin is %lf\n", sin(x));
-    while (x > PI + 0.1) x -= 2 * PI;
-    while (x < PI - 0.1) x += 2 * PI;
+    while (x > PI + 0.1)
+        x -= 2 * PI;
+    while (x < PI - 0.1)
+        x += 2 * PI;
     double z = x, xx = x;
     int i = 1;
-    while (myabs(z) >= eps) {
-        if ((i / 2) % 2 == 0) sum += z;
-        else sum -= z;
+    while (myabs(z) >= eps)
+    {
+        if ((i / 2) % 2 == 0)
+            sum += z;
+        else
+            sum -= z;
         fc *= (i + 1) * (i + 2);
         /* if (xx>max/(x*x) || (x*x)>max/(xx) || fc>max/((i+1)*(i+2)) || ((i+1)*(i+2))>max/fc)
          {
